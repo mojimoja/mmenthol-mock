36c950973f033f773c223003a352e95747da5af3
mojtabaparvar <mojtabaparvar92@gmail.com>
2023-06-10 17:52:33 +0330

    chore: initiate lazy loaded global saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useGlobalSlice } from "./store/globalSlice/globalSlice";
 
 function App() {
+  useGlobalSlice();
+
   return (
     <div className="App">
       <header className="App-header">
